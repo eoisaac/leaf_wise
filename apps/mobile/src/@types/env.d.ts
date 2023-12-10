e43f40c2aaa9b0ec228b87d26f9d0a1d0d1dc84a
Isaac Santiago <eoisaacc@gmail.com>
2023-12-09 22:05:15 -0300

    feat: create mqtt singleton

--- apps/mobile/src/@types/env.d.ts
+++ apps/mobile/src/@types/env.d.ts
@@ -8,6 +8,7 @@ declare module '@env' {
 
   export const MQTT_PROTOCOL: string | undefined
   export const MQTT_HOST: string | undefined
+  export const MQTT_CLIENT_ID: string | undefined
   export const MQTT_PORT: string | undefined
   export const MQTT_WS_PORT: string | undefined
   export const MQTT_USERNAME: string | undefined
